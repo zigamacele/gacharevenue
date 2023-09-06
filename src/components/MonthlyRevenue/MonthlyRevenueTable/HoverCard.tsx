3159d6da666661f54205f74275cd7e1514759fbb
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-09-06 07:46:04 +0200

    fix: hover card content under trigger

--- src/components/MonthlyRevenue/MonthlyRevenueTable/HoverCard.tsx
+++ src/components/MonthlyRevenue/MonthlyRevenueTable/HoverCard.tsx
@@ -1,5 +1,9 @@
 import ReleaseDate from '@/layouts/ReleaseDate'
-import { HoverCardContent, HoverCardTrigger } from '@/lib/shadcn/ui/hover-card'
+import {
+  HoverCardContent,
+  HoverCardTrigger,
+  HoverCard as HoverCardComp,
+} from '@/lib/shadcn/ui/hover-card'
 import { QueryOutput } from '@/types/supabase'
 import { useNavigate } from 'react-router-dom'
 
@@ -10,9 +14,17 @@ interface HoverOverProps {
 const HoverCard: React.FC<HoverOverProps> = ({ data }) => {
   const navigate = useNavigate()
   return (
-    <>
+    <HoverCardComp>
+      <HoverCardTrigger
+        className='cursor-pointer hover:opacity-60'
+        onClick={() => navigate(`/game/${data.id}`)}
+      >
+        <div className='w-20 truncate underline-offset-4 hover:underline hover:shadow-sm sm:w-[17.5em]'>
+          {data.en_name}
+        </div>
+      </HoverCardTrigger>
       <HoverCardContent className='flex flex-col gap-2'>
-        <div className='z-50 flex gap-2'>
+        <div className='flex gap-2'>
           <img
             src={data.icon}
             className='h-20 w-20 rounded border border-neutral-600 object-cover'
@@ -31,15 +43,7 @@ const HoverCard: React.FC<HoverOverProps> = ({ data }) => {
           </div>
         </div>
       </HoverCardContent>
-      <HoverCardTrigger
-        className='cursor-pointer hover:opacity-60'
-        onClick={() => navigate(`/game/${data.id}`)}
-      >
-        <div className='w-20 truncate underline-offset-4 hover:underline hover:shadow-sm sm:w-[17.5em]'>
-          {data.en_name}
-        </div>
-      </HoverCardTrigger>
-    </>
+    </HoverCardComp>
   )
 }
 
--- src/components/MonthlyRevenue/MonthlyRevenueTable/MonthlyRevTableRow.tsx
+++ src/components/MonthlyRevenue/MonthlyRevenueTable/MonthlyRevTableRow.tsx
@@ -5,12 +5,11 @@ import { getRegion } from '@/utils/region'
 import TrendArrow from '@/components/MonthlyRevenue/MonthlyRevenueTable/TrendArrow'
 import Tooltip from '@/components/Tooltip'
 import { CURRENT_TABLE, PREVIOUS_TABLE } from '@/constants/tables'
-import { HoverCard } from '@/lib/shadcn/ui/hover-card'
 import useBackgroundStore from '@/stores/background-store'
 import useMonthlyTableControls from '@/stores/monthly-table-controls'
 import { QueryOutput } from '@/types/supabase'
 import EditSection from './EditSection'
-import HoverCardComp from './HoverCard'
+import HoverCard from './HoverCard'
 import { cn } from '@/lib/shadcn/utils'
 
 interface MonthlyRevTableRowProps {
@@ -38,50 +37,48 @@ const MonthlyRevTableRow: React.FC<MonthlyRevTableRowProps> = ({
   const { setBackground } = useBackgroundStore()
 
   return (
-    <HoverCard>
-      <TableRow
-        className={cn('fade-in', isSectionRemoved && 'opacity-60')}
-        onMouseEnter={() => setBackground(data.background)}
-      >
-        <TableCell className='flex w-14 items-center justify-center gap-1'>
-          {showEditSection ? (
-            <EditSection data={data} />
-          ) : (
-            <TrendArrow change={(previousMonth[data.id] ?? 0) - index} />
-          )}
-        </TableCell>
-        {!isMobile && (
-          <TableCell className=' border-l border-r border-neutral-800 p-0'>
-            <img
-              src={data.background}
-              alt={data.en_name}
-              className='flex h-8 w-60 items-center object-cover'
-            />
-          </TableCell>
+    <TableRow
+      className={cn(isSectionRemoved && 'opacity-60')}
+      onMouseEnter={() => setBackground(data.background)}
+    >
+      <TableCell className='flex w-14 items-center justify-center gap-1'>
+        {showEditSection ? (
+          <EditSection data={data} />
+        ) : (
+          <TrendArrow change={(previousMonth[data.id] ?? 0) - index} />
         )}
-        <TableCell className={cn('text-center', region.color)}>
-          <Tooltip text={region.text}>{region.emoji}</Tooltip>
+      </TableCell>
+      {!isMobile && (
+        <TableCell className=' border-l border-r border-neutral-800 p-0'>
+          <img
+            src={data.background}
+            alt={data.en_name}
+            className='flex h-8 w-60 items-center object-cover'
+          />
         </TableCell>
-        <TableCell className='text-xs sm:text-sm'>
-          <HoverCardComp data={data} />
-        </TableCell>
-        <TableCell className='text-right text-neutral-200/80'>
-          {!isMobile
-            ? formatCurrency(previousRevenue)
-            : formatCurrencyCompact(previousRevenue)}
-        </TableCell>
-        <TableCell
-          className={cn(
-            'border-l border-neutral-800 bg-red-600 text-right',
-            currentRevenue >= previousRevenue && 'bg-green-600',
-          )}
-        >
-          {!isMobile
-            ? formatCurrency(currentRevenue)
-            : formatCurrencyCompact(currentRevenue)}
-        </TableCell>
-      </TableRow>
-    </HoverCard>
+      )}
+      <TableCell className={cn('text-center', region.color)}>
+        <Tooltip text={region.text}>{region.emoji}</Tooltip>
+      </TableCell>
+      <TableCell className='text-xs sm:text-sm'>
+        <HoverCard data={data} />
+      </TableCell>
+      <TableCell className='text-right text-neutral-200/80'>
+        {!isMobile
+          ? formatCurrency(previousRevenue)
+          : formatCurrencyCompact(previousRevenue)}
+      </TableCell>
+      <TableCell
+        className={cn(
+          'border-l border-neutral-800 bg-red-600 text-right',
+          currentRevenue >= previousRevenue && 'bg-green-600',
+        )}
+      >
+        {!isMobile
+          ? formatCurrency(currentRevenue)
+          : formatCurrencyCompact(currentRevenue)}
+      </TableCell>
+    </TableRow>
   )
 }
 
