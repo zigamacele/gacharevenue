cb9f75f9d9a774a616e7f23bc15d3d7f81a91b1f
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-10-16 18:42:11 +0200

    style: removed backdrop blur from alerts/notFound

--- src/layouts/PageNotFound.tsx
+++ src/layouts/PageNotFound.tsx
@@ -9,14 +9,14 @@ import { HOME } from '@/constants/links'
 const PageNotFound: React.FC = () => {
   return (
     <section className='w-sceen h-sceen slide-from-bottom mt-4 flex items-center justify-center'>
-      <div className='flex w-[90%] flex-col items-center justify-center gap-2 rounded border border-neutral-800 bg-neutral-950/80 px-6 py-5 backdrop-blur-[2px] transition-all sm:w-96'>
+      <div className='flex w-[90%] flex-col items-center justify-center gap-2 rounded border border-neutral-800 bg-neutral-900 px-6 py-5 sm:w-96'>
         <span className='text-6xl font-semibold sm:text-7xl'>404</span>
         <span className='opacity-60'>
           This page isn't available in your country.
         </span>
         <Separator className='opacity-40' />
         <Link to={HOME}>
-          <Button className='mt-1 flex items-center gap-1.5'>
+          <Button className='mt-2 flex items-center gap-1.5 bg-neutral-800'>
             <Undo2 width={16} />
             <span>Go back</span>
           </Button>
