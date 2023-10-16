5b2ff9ee99871bc8976bef2e5142329e66caab43
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-10-16 17:37:14 +0200

    feat: added reviews switch to individual game

--- src/components/Game/GameBody.tsx
+++ src/components/Game/GameBody.tsx
@@ -5,7 +5,6 @@ import Bar from '@/lib/nivo/Bar'
 import Line from '@/lib/nivo/Line'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/lib/shadcn/ui/tabs'
 
-import { CURRENT_TABLE, PREVIOUS_TABLE } from '@/constants/tables'
 import { prepareBarChartData, prepareLineChartData } from '@/utils/charts'
 
 import Tooltip from '../Tooltip'
@@ -18,6 +17,8 @@ interface GameBodyProps {
 }
 
 const GameBody: React.FC<GameBodyProps> = ({ currentGame, tables }) => {
+  const [content, setContent] = useState<string>('charts')
+
   const setTabsHelper = (tables: string[]) => {
     const newTabs = []
     for (const table of tables) {
@@ -29,56 +30,55 @@ const GameBody: React.FC<GameBodyProps> = ({ currentGame, tables }) => {
     return newTabs
   }
 
-  const [tabs, setTabs] = useState<string[]>(
-    setTabsHelper([PREVIOUS_TABLE, CURRENT_TABLE]),
-  )
-
   return (
-    <section>
+    <section className='relative'>
       <Tabs
-        defaultValue='last'
+        defaultValue={content}
         className='absolute z-50 flex w-40 flex-col items-start'
       >
         <TabsList className='center flex'>
-          <TabsTrigger
-            value='last'
-            onClick={() =>
-              setTabs(setTabsHelper([PREVIOUS_TABLE, CURRENT_TABLE]))
-            }
-          >
-            Last
+          <TabsTrigger value='charts' onClick={() => setContent('charts')}>
+            Charts
           </TabsTrigger>
           <TabsTrigger
-            value='all'
-            onClick={() => setTabs(setTabsHelper(tables))}
+            disabled
+            value='reviews'
+            onClick={() => setContent('reviews')}
           >
-            All
+            Reviews
           </TabsTrigger>
         </TabsList>
       </Tabs>
-      <Tabs
-        defaultValue='bar'
-        className='relative flex w-full flex-col items-end'
-      >
-        <TabsList className='center flex'>
-          <TabsTrigger value='bar'>
-            <Tooltip text='Bar Chart'>
-              <BarChart3 width={16} height={18} />
-            </Tooltip>
-          </TabsTrigger>
-          <TabsTrigger value='line'>
-            <Tooltip text='Line Chart'>
-              <LineChart width={16} height={18} />
-            </Tooltip>
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value='bar' className='h-80 w-full'>
-          <Bar data={prepareBarChartData([currentGame], tabs)} />
-        </TabsContent>
-        <TabsContent value='line' className='h-80 w-full'>
-          <Line data={prepareLineChartData([currentGame], tabs)} />
-        </TabsContent>
-      </Tabs>
+      {content === 'reviews' && <span>Coming soon</span>}
+      {content === 'charts' && (
+        <Tabs
+          defaultValue='bar'
+          className='relative flex w-full flex-col items-end'
+        >
+          <TabsList className='center flex'>
+            <TabsTrigger value='bar'>
+              <Tooltip text='Bar Chart'>
+                <BarChart3 width={16} height={18} />
+              </Tooltip>
+            </TabsTrigger>
+            <TabsTrigger value='line'>
+              <Tooltip text='Line Chart'>
+                <LineChart width={16} height={18} />
+              </Tooltip>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value='bar' className='h-80 w-full'>
+            <Bar
+              data={prepareBarChartData([currentGame], setTabsHelper(tables))}
+            />
+          </TabsContent>
+          <TabsContent value='line' className='h-80 w-full'>
+            <Line
+              data={prepareLineChartData([currentGame], setTabsHelper(tables))}
+            />
+          </TabsContent>
+        </Tabs>
+      )}
     </section>
   )
 }
