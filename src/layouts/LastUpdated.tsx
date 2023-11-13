079a8444a4eb5600ad1796d32d58187c9d82162d
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-11-13 16:16:53 +0100

    style: "last updated" badge

--- src/layouts/LastUpdated.tsx
+++ src/layouts/LastUpdated.tsx
@@ -1,3 +1,5 @@
+import { CalendarDays } from 'lucide-react'
+
 import Tooltip from '@/components/Tooltip.tsx'
 
 import useSupabaseStore from '@/stores/supabase-store.ts'
@@ -9,10 +11,12 @@ const LastUpdated: React.FC = () => {
   return (
     <Tooltip
       text='Last time database was updated'
-      className='slide-from-bottom fixed bottom-3 right-3 z-30 hidden items-center gap-2 rounded border border-neutral-700/60 bg-neutral-900/80 px-2.5 py-1 text-sm xl:flex'
+      className='slide-from-bottom group fixed bottom-3 right-3 z-30 hidden cursor-pointer items-center gap-2 rounded border border-neutral-700/60 bg-neutral-900/80 px-2.5 py-1 text-sm xl:flex'
     >
-      <p>Last updated:</p>
-      <p className='font-thin opacity-60'>{formatDate(lastUpdated)}</p>
+      <CalendarDays className='h-3.5 w-3.5 transition-opacity group-hover:opacity-60' />
+      <p className='text-sm font-thin opacity-60 transition-opacity group-hover:opacity-40'>
+        {formatDate(lastUpdated)}
+      </p>
     </Tooltip>
   )
 }
