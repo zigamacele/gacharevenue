75ff91a5233b8336df43d53ef790930ee2d8ce02
zigamacele <19758116+zigamacele@users.noreply.github.com>
2024-01-01 14:42:11 +0100

    style(HoverCard): publisher section, text overflow

--- src/components/Revenue/RevenueTable/HoverCard.tsx
+++ src/components/Revenue/RevenueTable/HoverCard.tsx
@@ -1,3 +1,4 @@
+import { BookCheck } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
 import {
@@ -23,9 +24,9 @@ const HoverCard: React.FC<HoverOverProps> = ({ data }) => {
         className='cursor-pointer hover:opacity-60'
         onClick={() => navigate(`/game/${data.id}`)}
       >
-        <div className='w-20 truncate underline-offset-4 hover:underline hover:shadow-sm sm:w-[17.5em]'>
+        <p className='w-20 truncate underline-offset-4 hover:underline hover:shadow-sm sm:w-[17.5em]'>
           {data.en_name}
-        </div>
+        </p>
       </HoverCardTrigger>
       <HoverCardContent className='flex gap-2'>
         <ImageComponent
@@ -36,17 +37,20 @@ const HoverCard: React.FC<HoverOverProps> = ({ data }) => {
           blurhash={data.blurhash}
           className='h-20 w-20 rounded border border-neutral-600 object-cover'
         />
-        <div className='items-between flex flex-col justify-between'>
-          <div className='flex flex-col gap-0.5'>
-            <span>{data.en_name}</span>
-            <span className='text-xs font-light opacity-80'>
-              {data.publisher}
+        <div className='items-between flex flex-col justify-between font-semibold'>
+          <p className='line-clamp-2'>{data.en_name}</p>
+          <div className='flex flex-col gap-1'>
+            <span className='flex items-center gap-2'>
+              <BookCheck size={16} />
+              <p className='w-24 truncate text-xs font-light opacity-80'>
+                {data.publisher}
+              </p>
             </span>
+            <ReleaseDate
+              releaseDate={data.release_date}
+              className='truncate text-xs font-light'
+            />
           </div>
-          <ReleaseDate
-            releaseDate={data.release_date}
-            className='truncate text-xs font-light'
-          />
         </div>
       </HoverCardContent>
     </HoverCardComp>
